1c0a3c74e297a974854950d370a86f272f333047
patpbe <pat.pbe@instanweb.fr>
2017-01-09 13:00:24 +0100

    update test.rb 3x salut (v2))

--- test.rb
+++ test.rb
@@ -1,8 +1,6 @@
-name = gets()
+name = gets().chomp
 puts 'hello' << ' ' << name
 puts "hello #{name}"
 puts "comment vas-tu ?"
-puts "salut #{name}"
-[1,2,3].each do |i| 
-    puts "salut!"
-end
+3.times {puts "salut #{name} !"}
+
