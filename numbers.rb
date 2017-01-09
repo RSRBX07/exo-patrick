81a60113aeca5582171aa09c6ea7cb7823fe1c8a
patpbe <pat.pbe@instanweb.fr>
2017-01-09 15:08:04 +0100

    update numbers.rb (v5)

--- numbers.rb
+++ numbers.rb
@@ -5,7 +5,15 @@ nombre_secondes = 365*24*3600
 puts "nombre de secondes par an (hors année bissextile)= #{nombre_secondes}"
 
 #calcul de l'age en secondes'
-now = Time.now.to_i
-birth = Time.new(1963, 12, 26, 6, 0, 0, 0).to_i
-age_secondes=now-birth
+now = Time.now
+birth = Time.new(1963, 12, 26)
+birth2 = Time.new("1963-12-26T06:00:00 +0200")
+age_secondes=now-birth2
 puts "age=#{age_secondes} secondes"
+puts "age=#{now-birth} secondes"
+
+origin=Time.new(0)
+test=Time.new(1,1,1)
+p(origin)
+p(test)
+p((test-origin).to_i)
