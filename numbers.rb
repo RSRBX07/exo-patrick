9f0174108973c865e0a1d18dc6242adcc82f4552
patpbe <pat.pbe@instanweb.fr>
2017-01-09 14:29:49 +0100

    add  nimbers.rb

---
+++ numbers.rb
@@ -0,0 +1,8 @@
+#exercices sur les nombres
+
+#calcul nombre de secondes en 1 année
+nombre_secondes = 365*24*3600
+puts nombre_secondes
+
+#calcul de l'age en secondes'
+age_secondes=53*nombre_secondes
