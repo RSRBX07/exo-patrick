0933c8582194e57867a7e822262c11fbe59de67c
patpbe <pat.pbe@instanweb.fr>
2017-01-10 16:26:08 +0100

    song first release

---
+++ song.rb
@@ -0,0 +1,27 @@
+=begin
+Ma poul' n'a plus qu' vingt-neuf poussins, 
+Ma poul' n'a plus qu' vingt-neuf poussins,
+Elle en avait trente.
+Allongeons la jambe,
+Allongeons la jambe, la jambe
+Car la route est longue.
+Allongeons la jambe, la jambe
+Car la route est longue.
+=end
+iterations=(0..29).to_a.reverse
+iterations.each do |i|
+    if i > 1
+        puts "Ma poul' n'a plus qu' #{i} poussins, " * 2
+    elsif i == 1
+        puts "Ma poul' n'a plus qu'un seul poussin, " * 2
+    else
+        puts "Ma poul' n'a plus aucun poussin, " * 2
+    end
+    puts "Elle en avait trente"
+    puts "Allongeons la jambe,"
+    (1..2).each do
+        puts "Allongeons la jambe, la jambe"
+        puts "Car la route est longue."    
+    end
+    puts
+end
