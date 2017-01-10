371a208e678fd05d63f7b15080420bc7e2f33893
Patrick <pat.pbe@instanweb.fr>
2017-01-10 20:16:43 +0100

    loto.rb v9

--- loto.rb
+++ loto.rb
@@ -8,7 +8,7 @@ end until lotto_board.length == 5
 #grille jouée
 puts "Grille #{lotto_board.join ','}"
 #tirage du loto
-lotto_draw = (1..49).to_a.each{|n|n+1}.shuffle.take(5).sort
+lotto_draw = (1..49).to_a.shuffle.take(5).sort
 puts "Tirage #{lotto_draw.join ','}"
 #numéros gagnants
 winning_number = lotto_draw & lotto_board
