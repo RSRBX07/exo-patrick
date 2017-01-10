bf57758e1bcbd7a67394ede8ba711ed3b7e7bb42
patpbe <pat.pbe@instanweb.fr>
2017-01-10 10:48:05 +0100

    update loto.rb (v5)

--- loto.rb
+++ loto.rb
@@ -1,3 +1,3 @@
 #afficher un tirage de 5 numéros du loto entre 1 et 49
-tirage=Array.new(49){|n|n+1}.shuffle.take(5).sort
+tirage=(1..49).to_a.shuffle.take(5).sort
 print "Tirage : #{tirage.join ', '}"
