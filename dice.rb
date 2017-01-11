4de92c9047b894b631cbea1cf2da6b46382b7e67
patpbe <pat.pbe@instanweb.fr>
2017-01-11 16:29:46 +0100

    dice correction faute orthographe méthode roll

--- dice.rb
+++ dice.rb
@@ -1,12 +1,12 @@
 #écrire une méthode qui retourne le tirage d'un dé
-def roll (cheatted=false)
-    if cheatted
-        cheatted
+def roll cheated=false
+    if cheated
+        cheated
     else
         [1,2,3,4,5,6].shuffle[0]
     end
 end
 
 #afficher le résultat d'un tirage de dé à 6 faces'
-print roll 2
-print roll
+puts roll 2
+puts roll
