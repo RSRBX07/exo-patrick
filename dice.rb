1b3ea50fc1dbe0c1fa278570a10fe6fac0fc3d3b
patpbe <pat.pbe@instanweb.fr>
2017-01-11 16:41:52 +0100

    dice correction roll avec dé pipé ou non

--- dice.rb
+++ dice.rb
@@ -1,10 +1,6 @@
 #écrire une méthode qui retourne le tirage d'un dé
 def roll cheated=false
-    if cheated
-        cheated
-    else
-        [1,2,3,4,5,6].shuffle[0]
-    end
+    cheated ? cheated : [1,2,3,4,5,6].shuffle[0]
 end
 
 #afficher le résultat d'un tirage de dé à 6 faces'
