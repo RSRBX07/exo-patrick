e88ab3a75da065bf19db6c8b485829d70295ce88
patpbe <pat.pbe@instanweb.fr>
2017-01-11 16:26:33 +0100

    dice.rb modification méthode roll pour tricherie

--- dice.rb
+++ dice.rb
@@ -1,7 +1,12 @@
-#écrire une méthode qui retourne le contenu d'un dé
-def roll
-    [1,2,3,4,5,6].shuffle[0]
+#écrire une méthode qui retourne le tirage d'un dé
+def roll (cheatted=false)
+    if cheatted
+        cheatted
+    else
+        [1,2,3,4,5,6].shuffle[0]
+    end
 end
 
 #afficher le résultat d'un tirage de dé à 6 faces'
+print roll 2
 print roll
