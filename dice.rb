2257f443cffbc8410462ec519df82c6413972bed
patpbe <pat.pbe@instanweb.fr>
2017-01-11 16:03:07 +0100

    définition méthode roll

--- dice.rb
+++ dice.rb
@@ -1,6 +1,7 @@
-#afficher le résultat d'un tirage de dé à 6 faces'
-faces=[1,2,3,4,5,6]
-10.times {print faces[rand 6],' '}
-puts
-10.times {print faces.shuffle[0],' '}
+#écrire une méthode qui retourne le contenu d'un dé
+def roll
+    [1,2,3,4,5,6].shuffle[0]
+end
 
+#afficher le résultat d'un tirage de dé à 6 faces'
+print roll
