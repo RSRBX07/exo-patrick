2d12850ceaa6bbfa7dfe4f713e5b22281fb166fb
patpbe <pat.pbe@instanweb.fr>
2017-01-12 10:14:48 +0100

    dice dice avec retour en lettre intro classe Hash

--- dice.rb
+++ dice.rb
@@ -1,8 +1,8 @@
 #écrire une méthode qui retourne le tirage d'un dé
 def roll cheated=false
-    cheated ? cheated : [1,2,3,4,5,6].shuffle[0]
+    cheated ? cheated : {1=>'un',2=>'deux',3=>'trois',4=>'quatre',5=>'cinq',6=>'six'}.to_a.shuffle[0][1]
 end
 
 #afficher le résultat d'un tirage de dé à 6 faces'
-puts roll 2
+puts roll "trois"
 puts roll
