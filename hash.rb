d356a3042ead53c3e93906a9130730806bd67f51
patpbe <pat.pbe@instanweb.fr>
2017-01-12 12:05:53 +0100

    hash conversion chaines en symboles

--- hash.rb
+++ hash.rb
@@ -1,16 +1,16 @@
 meta={
-    'place' =>'Roubaix',
-    'author'=>'Patrick'
+    place:  :Roubaix,
+    author: :Patrick
 }
 zipcodes={
-    'roubaix'  => 59100,
-    'ennevelin'=> 59710,
-    'lille'    => 59000,
-    'toulon'   => 83000
+    roubaix:   59100,
+    ennevelin: 59710,
+    lille:     59000,
+    toulon:    83000
 }
 
 print "Donner moi une ville : "
-state=gets.chomp.downcase
+state=gets.chomp.downcase.to_sym
 print"Vous habitez #{state}"
 code=zipcodes[state]
 puts code ? " code postal #{code}" : ", mais je ne connais pas son code postal"
@@ -20,5 +20,5 @@ if code
   puts "vérification : une ville correspondant à #{code} est #{verify_town}"
 end
 
-puts "made in @#{meta['place']} by #{meta['author']}"
+puts "made in @#{meta[:place]} by #{meta[:author]}"
 
