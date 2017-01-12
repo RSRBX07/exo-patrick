76624025f832a721f7efc0c406370408a6b135f2
patpbe <pat.pbe@instanweb.fr>
2017-01-12 11:25:23 +0100

    hash.rb codes postaux bi-directionnels

--- hash.rb
+++ hash.rb
@@ -14,6 +14,11 @@ state=gets.chomp.downcase
 print"Vous habitez #{state}"
 code=zipcodes[state]
 puts code ? " code postal #{code}" : ", mais je ne connais pas son code postal"
+if code
+  ziptowns=zipcodes.invert
+  verify_town=ziptowns[code]
+  puts "vérification : une ville correspondant à #{code} est #{verify_town}"
+end
 
 puts "made in @#{meta['place']} by #{meta['author']}"
 
