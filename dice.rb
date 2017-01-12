6dd4e02ca3e8523fb7795cf741b3d5ddd871f8b3
patpbe <pat.pbe@instanweb.fr>
2017-01-12 10:59:34 +0100

    dice.rb en lettres (mise en forme)

--- dice.rb
+++ dice.rb
@@ -1,6 +1,12 @@
 #écrire une méthode qui retourne le tirage d'un dé
 def roll cheated=false
-    cheated ? cheated : {1=>'un',2=>'deux',3=>'trois',4=>'quatre',5=>'cinq',6=>'six'}.to_a.shuffle[0][1]
+    cheated ? cheated : {1=>'un',
+                         2=>'deux',
+                         3=>'trois',
+                         4=>'quatre',
+                         5=>'cinq',
+                         6=>'six'}
+                         .to_a.shuffle[0][1]
 end
 
 #afficher le résultat d'un tirage de dé à 6 faces'
