491718c290d9643d726474a947958e78145660a5
patpbe <pat.pbe@instanweb.fr>
2017-01-12 15:09:13 +0100

    6th release with range 0..19

--- french_number.rb
+++ french_number.rb
@@ -3,13 +3,14 @@
 # - qui retourne la chaîne de caractère du nombre écrit en toutes lettres
 
 def num_to_string number
-    if number.class == Fixnum && number >= 0 && number <= 9
-        ["zero","un","deux","trois","quatre","cinq","six","sept","huit","neuf"][number]
+    if number.class == Fixnum and number >=0 and number <= 19
+        ["zero","un","deux","trois","quatre","cinq","six","sept","huit","neuf",
+         "dix","onze","douze","treize","quatorze","quinze","seize","dix-sept","dix-huit","dix-neuf"][number]
     else
-        raise ArgumentError, "#{number} is not a Fixnum or is not in 0..9"
+        raise ArgumentError, "#{number} is not a Fixnum or is not in 0..19"
     end
 end
 
-(0..9).each {|n| puts num_to_string n}
-puts num_to_string 10
+(0..19).each {|n| puts num_to_string n}
+puts num_to_string 20
 puts num_to_string 'toto'
