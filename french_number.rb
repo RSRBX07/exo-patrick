e9603b90d90e30a5cdfacf2045fc042bf43ef552
patpbe <pat.pbe@instanweb.fr>
2017-01-12 14:39:57 +0100

    4th release with array without check

--- french_number.rb
+++ french_number.rb
@@ -3,31 +3,8 @@
 # - qui retourne la chaîne de caractère du nombre écrit en toutes lettres
 
 def num_to_string number
-    case number
-    when 0
-        "zero"
-    when 1
-        "un"
-    when 2
-        "deux"
-    when 3
-        "trois"
-    when 4
-        "quatre"
-    when 5
-        "cinq"
-    when 6
-        "six"
-    when 7
-        "sept"
-    when 8
-        "huit"
-    when 9
-        "neuf"
-    else
-        raise ArgumentError, "chiffre inconnu"
-    end
+    ["zero","un","deux","trois","quatre","cinq","six","sept","huit","neuf"][number]
 end
 
 (0..9).each {|n| puts num_to_string n}
-puts num_to_string 'toto'
+
