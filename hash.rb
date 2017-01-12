7653f343edccf26d321738783cb1c22a8e15617b
patpbe <pat.pbe@instanweb.fr>
2017-01-12 11:10:52 +0100

    hash.rb test hash sur codes postaux

--- hash.rb
+++ hash.rb
@@ -2,5 +2,18 @@ meta={
     'place' =>'Roubaix',
     'author'=>'Patrick'
 }
+zipcodes={
+    'roubaix'  => 59100,
+    'ennevelin'=> 59710,
+    'lille'    => 59000,
+    'toulon'   => 83000
+}
+
+print "Donner moi une ville : "
+state=gets.chomp.downcase
+print"Vous habitez #{state}"
+code=zipcodes[state]
+puts code ? " code postal #{code}" : ", mais je ne connais pas son code postal"
 
 puts "made in @#{meta['place']} by #{meta['author']}"
+
