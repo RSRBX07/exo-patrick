3e3de8d4e2d5669dbecdf1e9f016e7d8efc059d6
patpbe <pat.pbe@instanweb.fr>
2017-01-12 16:12:12 +0100

    7th release with range 0..99

--- french_number.rb
+++ french_number.rb
@@ -2,15 +2,35 @@
 # - qui prend une instance de Fixnum en argument entre 0 et 9
 # - qui retourne la chaîne de caractère du nombre écrit en toutes lettres
 
-def num_to_string number
-    if number.class == Fixnum and number >=0 and number <= 19
-        ["zero","un","deux","trois","quatre","cinq","six","sept","huit","neuf",
-         "dix","onze","douze","treize","quatorze","quinze","seize","dix-sept","dix-huit","dix-neuf"][number]
+def num_to_string number, first_call=true
+    if number==0 && first_call==false
+        return ""
+    end
+    string = first_call ? "" : "-"
+    if number.class == Fixnum
+        case number
+        when 0..16
+            if first_call==false && (number==1||number==11)
+                string += "et-"
+            end
+            string += ["zero","un","deux","trois","quatre","cinq","six","sept","huit","neuf",
+                       "dix","onze","douze","treize","quatorze","quinze","seize"][number]
+        when 17..19
+            string += "dix" + num_to_string(number-10, false)
+        when 20..99
+            dizaine = (number-20)/10
+            unite = (number-20)%10
+            if dizaine == 5 || dizaine == 7
+                dizaine -=1
+                unite += 10
+            end
+            string += ["vingt","trente","quarante","cinquante","soixante",nil,"quatre-vingt"][dizaine]+num_to_string(unite, false)
+        end
     else
-        raise ArgumentError, "#{number} is not a Fixnum or is not in 0..19"
+        raise ArgumentError, "#{number} is not a Fixnum"
     end
 end
 
-(0..19).each {|n| puts num_to_string n}
-puts num_to_string 20
-puts num_to_string 'toto'
+(0..99).each {|n| puts num_to_string n}
+#puts num_to_string 20
+#puts num_to_string 'toto'
