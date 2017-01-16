b93072d80740d7ebc631ec7a0654e80493bd39d1
patpbe <pat.pbe@instanweb.fr>
2017-01-16 10:17:24 +0100

    dice with class

--- dice.rb
+++ dice.rb
@@ -1,17 +1,18 @@
 class Dice
+    #écrire une méthode qui retourne le tirage d'un dé
+    def roll cheated=false
+        cheated ? cheated : {1=>'un',
+                            2=>'deux',
+                            3=>'trois',
+                            4=>'quatre',
+                            5=>'cinq',
+                            6=>'six'}
+                            .to_a.shuffle[0][1]
+    end
 end
 
-#écrire une méthode qui retourne le tirage d'un dé
-def roll cheated=false
-    cheated ? cheated : {1=>'un',
-                         2=>'deux',
-                         3=>'trois',
-                         4=>'quatre',
-                         5=>'cinq',
-                         6=>'six'}
-                         .to_a.shuffle[0][1]
-end
 
 #afficher le résultat d'un tirage de dé à 6 faces'
-puts roll "trois"
-puts roll
+dice = Dice.new
+puts dice.roll "trois"
+puts dice.roll
