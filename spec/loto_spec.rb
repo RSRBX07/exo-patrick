a2539a9b98bfc767abbfce56cc9efb88b153292e
patpbe <pat.pbe@instanweb.fr>
2017-01-17 15:31:26 +0100

    rspec prêt à tester

---
+++ spec/loto_spec.rb
@@ -0,0 +1,38 @@
+require './loto/loto.rb'
+
+#afficher le résultat d'un tirage de dé à 6 faces'
+# dice = Dice.new
+# puts dice.roll "trois"
+# puts dice.roll
+
+# number = rand(999999999)
+# puts "number:#{number} soit #{number.spell_to_french}"
+
+# loto
+# loto = Loto.new
+
+# puts loto.lotto_boards.inspect
+# # choisir plusieurs grilles
+# (1..2).each do |n|
+#     puts "Jeu:#{loto.create_lotto_board Loto.random_board}"
+#     puts loto.lotto_boards.inspect
+# end
+
+#puts "Jeu:#{loto.create_lotto_board.join ','}"
+#puts "Jeu:#{loto.create_lotto_board.join ','}"
+#puts "Jeu:#{loto.create_lotto_board.join ','}"
+# faire 1000 tirages tant qu'on ne gagne pas'
+# (1..2).each do |n|
+#     puts "Tirage #{n} : #{loto.do_lotto_draw.join ','}"
+#     break if loto.win?
+# end
+# puts "#{loto.boards_count} grilles jouées"
+# puts "Dernier résultat du loto : #{loto.lotto_draw}"
+# puts loto.win? ? "Loto : numéro(s) gagnant(s) #{loto.lotto_winning_boards}" : "Loto perdu"
+
+
+# puts "Le tirage a déjà eu lieu" if loto.played?
+
+# if loto.create_lotto_board(Loto.random_board) == false
+#     puts "Impossible d'ajouter une grille : le tirage a déjà eu lieu"
+# end
