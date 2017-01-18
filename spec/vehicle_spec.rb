9d9281b16517a9a9a4db2e0a69d96b4384fbf617
patpbe <pat.pbe@instanweb.fr>
2017-01-18 13:56:13 +0100

    define first rspec for vehicle

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../vehicle/vehicle.rb'
+
+RSpec.describe Vehicle do
+  it 'is in a defined position' do
+    vehicle = Vehicle.new
+    expect(vehicle.position).not_to be_nil
+    expect(vehicle.position.length).to be > 0
+  end
+
+  it 'move' do
+    vehicle = Vehicle.new
+    vehicle.move 'Lille'
+    expect(vehicle.position).not_to be_nil
+    expect(vehicle.position.length).to be > 0
+  end
+end
+
