5171312453a25a8857bc1869cb7821f7fb1d8bec
patpbe <pat.pbe@instanweb.fr>
2017-01-19 16:05:05 +0100

    counter by class (optimized v1) work in progress

--- vehicle/counter.rb
+++ vehicle/counter.rb
@@ -1,15 +1,8 @@
 class Counter
 
-  def self.get_filename
-    dirname = File.join(File.dirname(__FILE__), '../tmp/')
-    Dir::mkdir dirname unless File.exist? dirname
-    dirname + 'counter.txt'
-  end
-
   def self.add_one a_class
-    count = value(a_class) + 1
     counters = self.read_all
-    counters[a_class.to_s] = count
+    counters.key? a_class.to_s ? counters[a_class.to_s] += 1 : counters[a_class.to_s] = 1
     file = File.open(get_filename, 'w')
     counters.each do |e|
       file.puts(e[0] + '=' + e[1].to_s)
@@ -17,24 +10,27 @@ class Counter
     file.close
   end
 
+  def self.value a_class
+    counters = self.read_all
+    return 0 unless counters.key? a_class.to_s
+    counters[a_class.to_s] 
+  end
+
+  private
+  def self.get_filename
+    dirname = File.join(File.dirname(__FILE__), '../tmp/')
+    Dir::mkdir dirname unless File.exist? dirname
+    dirname + 'counter.txt'
+  end
+  
   def self.read_all
     return {} unless File.exist? get_filename
     counters = {}
     File.open(get_filename, 'r').each_line do |line|
       values = line.split '='
-      counters[values[0]] = values[1]
+      counters[values[0]] = values[1].to_i
     end
     counters
   end
-
-  def self.value a_class
-    return 0 unless File.exist? get_filename
-    File.open(get_filename, 'r').each_line do |line|
-      values = line.split '='
-      if values[0] == a_class.to_s
-        return values[1].to_i
-      end
-    end
-    0
-  end
+  
 end
