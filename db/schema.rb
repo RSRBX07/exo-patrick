872a23c24ae7ab53f9d9b4aa419e11dc3683f311
patpbe <pat.pbe@instanweb.fr>
2017-01-23 16:44:37 +0100

    add players to game

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123140728) do
+ActiveRecord::Schema.define(version: 20170123154328) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "players"
   end
 
   create_table "people", force: :cascade do |t|
