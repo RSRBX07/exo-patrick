27397dc8bb3bc697d3006e9f5dc8a639c463d758
patpbe <pat.pbe@instanweb.fr>
2017-01-24 15:56:15 +0100

    add draw field

---
+++ db/migrate/20170124144830_add_draw_to_game.rb
@@ -0,0 +1,5 @@
+class AddDrawToGame < ActiveRecord::Migration[5.0]
+  def change
+    add_column :games, :draw, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124095125) do
+ActiveRecord::Schema.define(version: 20170124144830) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "draw"
   end
 
   create_table "people", force: :cascade do |t|
