bb7e00b4283002bd15cd23f8ef3481c64463d1be
patpbe <pat.pbe@instanweb.fr>
2017-01-24 11:48:59 +0100

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Game.destroy_all
-Game.create([{name:'Loto du lundi'}, {description:'Le tirage est 12, 42, 11, 6 et 33'}])
-Game.create([{name:'Loto du mardi'}, {description:'Le tirage est 13, 42, 11, 6 et 33'}])
-Game.create([{name:'Loto du mercredi'}, {description:'Le tirage est 14, 42, 11, 6 et 33'}])
-Game.create([{name:'Loto du jeudi'}, {description:'Le tirage est 15, 42, 11, 6 et 33'}])
-Game.create([{name:'Loto du vendredi'}, {description:'Le tirage est 16, 42, 11, 6 et 33'}])
+Game.create([
+  {name:'Loto du lundi', description:'Le tirage est 12, 42, 11, 6 et 33'},
+  {name:'Loto du mardi', description:'Le tirage est 13, 42, 11, 6 et 33'},
+  {name:'Loto du mercredi',description:'Le tirage est 14, 42, 11, 6 et 33'},
+  {name:'Loto du jeudi', description:'Le tirage est 15, 42, 11, 6 et 33'},
+  {name:'Loto du vendredi', description:'Le tirage est 16, 42, 11, 6 et 33'}])
